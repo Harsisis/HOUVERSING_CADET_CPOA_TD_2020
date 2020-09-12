f4e70a79a60f7edf116e7bbe08bd93d4addf57b6
Harsisis <56965488+Harsisis@users.noreply.github.com>
2020-09-12 20:05:42 +0200

    I've started the app to navigate and reach each request

---
+++ src/main/ui/applimenu.java
@@ -0,0 +1,90 @@
+package main.ui;
+import java.util.Scanner;
+
+public class applimenu {
+    Scanner scan = new Scanner(System.in);
+    public applimenu() {
+        menu1();
+    }
+
+    //menu to manage if the user want a category, a product or a customer
+    public void menu1(){
+        System.out.println("Hello, what do you want to do ?\n\t1. Category\n\t2. Product\n\t3. Customer");
+        String choice;
+        do {
+            System.out.println("Select a number between 1 and 3 please");
+            choice = scan.next();
+            System.out.println(choice);
+        } while(choice.equals("1") && choice.equals("2") && choice.equals("3"));
+
+        switch(choice){
+            case "1": menuCat();
+                break;
+            case "2": menuProd();
+                break;
+            case "3": menuCust();
+                break;
+        }
+    }
+
+    //menu to manage Customers
+    private void menuCust() {
+        System.out.println("What do you want to do now ?\n\t1. Add a Customer\n\t2. Edit a Customer\n\t3. Disp all the Customers");
+        String choice;
+        do {
+            System.out.println("Select a number between 1 and 3 please");
+            choice = scan.next();
+            System.out.println(choice);
+        } while(choice.equals("1") && choice.equals("2") && choice.equals("3"));
+
+        switch(choice){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+        }
+    }
+
+    //menu to manage Products
+    private void menuProd() {
+            System.out.println("What do you want to do now ?\n\t1. Add a Product\n\t2. Edit a Product\n\t3. Disp all the Products");
+        String choice;
+        do {
+            System.out.println("Select a number between 1 and 3 please");
+            choice = scan.next();
+            System.out.println(choice);
+        } while(choice.equals("1") && choice.equals("2") && choice.equals("3"));
+
+        switch(choice){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+        }
+    }
+
+    //menu to manage Categories
+    private void menuCat() {
+        System.out.println("What do you want to do now ?\n\t1. Add a Category\n\t2. Edit a Category\n\t3. Disp all the Categories");
+        String choice;
+        do {
+            System.out.println("Select a number between 1 and 3 please");
+            choice = scan.next();
+            System.out.println(choice);
+        } while(choice.equals("1") && choice.equals("2") && choice.equals("3"));
+
+        switch(choice){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+        }
+    }
+
+}
