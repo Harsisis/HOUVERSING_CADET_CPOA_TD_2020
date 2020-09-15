c9d4220416461dd949ea943c5220d5002921a7a9
Harsisis <56965488+Harsisis@users.noreply.github.com>
2020-09-15 23:24:10 +0200

    fixes on query methods in every query files

--- src/main/db/Customer.java
+++ src/main/db/Customer.java
@@ -1,80 +1,75 @@
-//package main.db;
-//import java.util.Scanner;
-//import java.sql.*;
-//
-//public class Customer {
-//    Scanner scan = new Scanner(System.in);
-//
-//    public void add_cust(){
-//        String name_cust = null;
-//        String surname_cust = null;
-//        System.out.println("Prompt the customer name :\n");
-//        name_cust = scan.next();
-//        System.out.println("Prompt the customer surname :\n");
-//        surname_cust = scan.next();
-//        try{
-//            Statement requete = Connection.getInstance().getConnection().createStatement();
-//            ResultSet res = requete.executeQuery("INSERT INTO Client (nom, prenom) VALUES('" + name_cust + "','" + surname_cust + "')");
-//            if (res != null)
-//                res.close();
-//            if (requete != null)
-//                requete.close();
-//            if (Connection.getInstance() != null)
-//                Connection.getInstance().getConnection().close();
-//        } catch (SQLException sqle) {
-//            System.out.println("Pb select" + sqle.getMessage());
-//        }
-//    }
-//
-//    public void edit_cust(){
-//        String name_cust = null;
-//        String surname_cust = null;
-//        String id_cust = null;
-//        System.out.println("Wich customer would you change ?\n");
-//        id_cust = scan.next();
-//        System.out.println("Prompt the customer name :\n");
-//        name_cust = scan.next();
-//        System.out.println("Prompt the customer surname :\n");
-//        surname_cust = scan.next();
-//        try{
-//            Statement requete = Connection.getInstance().getConnection().createStatement();
-//            ResultSet res = requete.executeQuery("UPDATE Client SET nom='" + name_cust + "', prenom='" + surname_cust + "' WHERE id_produit=" + id_cust);
-//            if (res != null)
-//                res.close();
-//            if (requete != null)
-//                requete.close();
-//            if (Connection.getInstance() != null)
-//                Connection.getInstance().getConnection().close();
-//        } catch (SQLException sqle) {
-//            System.out.println("Pb select" + sqle.getMessage());
-//        }
-//    }
+package main.db;
+import java.util.Scanner;
+import java.sql.*;
+
+public class Customer {
+    Scanner scan = new Scanner(System.in);
+
+    public void add_cust(){
+        String name_cust = null;
+        String surname_cust = null;
+        System.out.println("Prompt the customer name :\n");
+        name_cust = scan.next();
+        System.out.println("Prompt the customer surname :\n");
+        surname_cust = scan.next();
+        java.sql.Connection connection = Connection.connect();
+        try{
+            String requete = "INSERT INTO Categorie(nom, prenom) VALUES(?, ?)";
+            PreparedStatement ps = connection.prepareStatement(requete);
+            ps.setString(1, name_cust);
+            ps.setString(2, surname_cust);
+            ps.executeUpdate();
+            connection.close();
+        } catch (SQLException sqle) {
+            System.out.println("Pb select" + sqle.getMessage());
+        }
+    }
+
+    public void edit_cust(){
+        String name_cust = null;
+        String surname_cust = null;
+        String id_cust = null;
+        System.out.println("Wich customer would you change ?\n");
+        id_cust = scan.next();
+        System.out.println("Prompt the customer name :\n");
+        name_cust = scan.next();
+        System.out.println("Prompt the customer surname :\n");
+        surname_cust = scan.next();
+        java.sql.Connection connection = Connection.connect();
+        try{
+            String requete = "UPDATE Client SET nom=?, prenom=? WHERE id_client=? ";
+            PreparedStatement ps = connection.prepareStatement(requete);
+            ps.setString(1, name_cust);
+            ps.setString(2, surname_cust);
+            ps.setString(3, id_cust);
+            ps.executeUpdate();
+            connection.close();
+        } catch (SQLException sqle) {
+            System.out.println("Pb select" + sqle.getMessage());
+        }
+    }
 //
 //    public void all_cust(){
 //
 //        try{
-//            Statement requete = Connection.getInstance().getConnection().createStatement();
-//            ResultSet res = requete.executeQuery("SELECT * FROM Client");
+//            java.sql.Connection connection = Connection.connect();
+//            String requete = "SELECT * FROM Client";
+//            PreparedStatement ps = connection.prepareStatement(requete);
 //
-//            ResultSetMetaData rsmd = res.getMetaData();
+//            ResultSetMetaData rsmd = ps.getMetaData();
 //            int columnsNumber = rsmd.getColumnCount();
-//            while (res.next()) {
+//            while (.next()) {
 //                for (int i = 1; i <= columnsNumber; i++) {
 //                    if (i > 1) System.out.print(",  ");
-//                    String columnValue = res.getString(i);
+//                    String columnValue = .getString(i);
 //                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
 //                }
 //                System.out.println("");
 //            }
-//            if (res != null)
-//                res.close();
-//            if (requete != null)
-//                requete.close();
-//            if (Connection.getInstance() != null)
-//                Connection.getInstance().getConnection().close();
+//            connection.close();
 //        } catch (SQLException sqle) {
 //            System.out.println("Pb select" + sqle.getMessage());
 //        }
 //
 //    }
-//}
+}
--- src/main/db/Product.java
+++ src/main/db/Product.java
@@ -1,66 +1,69 @@
-//package main.db;
-//import java.util.Scanner;
-//import java.sql.*;
-//
-//public class Product {
-//    Scanner scan = new Scanner(System.in);
-//
-//    public void add_prod(){
-//        String name_prod = null;
-//        String description_prod = null;
-//        String price_prod = null;
-//        String visual_prod = null;
-//        System.out.println("Prompt the product name :\n");
-//        name_prod = scan.next();
-//        System.out.println("Prompt the product description :\n");
-//        description_prod = scan.next();
-//        System.out.println("Prompt the product price :\n");
-//        price_prod = scan.next();
-//        System.out.println("Prompt the product visual :\n");
-//        visual_prod = scan.next();
-//        try{
-//            Statement requete = Connection.getInstance().getConnection().createStatement();
-//            ResultSet res = requete.executeQuery("INSERT INTO Produit (nom, description, tarif, visuel) VALUES('" + name_prod + "','" + description_prod + "','" + price_prod + "','" + visual_prod + "')");
-//            if (res != null)
-//                res.close();
-//            if (requete != null)
-//                requete.close();
-//            if (Connection.getInstance() != null)
-//                Connection.getInstance().getConnection().close();
-//        } catch (SQLException sqle) {
-//            System.out.println("Pb select" + sqle.getMessage());
-//        }
-//    }
-//
-//    public void edit_prod(){
-//        String name_prod = null;
-//        String description_prod = null;
-//        String price_prod = null;
-//        String visual_prod = null;
-//        String id_prod = null;
-//        System.out.println("Wich product would you change ?\n");
-//        id_prod = scan.next();
-//        System.out.println("Prompt the product name :\n");
-//        name_prod = scan.next();
-//        System.out.println("Prompt the product description :\n");
-//        description_prod = scan.next();
-//        System.out.println("Prompt the product price :\n");
-//        price_prod = scan.next();
-//        System.out.println("Prompt the product visual :\n");
-//        visual_prod = scan.next();
-//        try{
-//            Statement requete = Connection.getInstance().getConnection().createStatement();
-//            ResultSet res = requete.executeQuery("UPDATE Produit SET nom='" + name_prod + "', description='" + description_prod +"', tarif='" + price_prod + "', visuel='" + visual_prod +"' WHERE id_produit=" + id_prod);
-//            if (res != null)
-//                res.close();
-//            if (requete != null)
-//                requete.close();
-//            if (Connection.getInstance() != null)
-//                Connection.getInstance().getConnection().close();
-//        } catch (SQLException sqle) {
-//            System.out.println("Pb select" + sqle.getMessage());
-//        }
-//    }
+package main.db;
+import java.util.Scanner;
+import java.sql.*;
+
+public class Product {
+    Scanner scan = new Scanner(System.in);
+
+    public void add_prod(){
+        String name_prod = null;
+        String description_prod = null;
+        String price_prod = null;
+        String visual_prod = null;
+        System.out.println("Prompt the product name :\n");
+        name_prod = scan.next();
+        System.out.println("Prompt the product description :\n");
+        description_prod = scan.next();
+        System.out.println("Prompt the product price :\n");
+        price_prod = scan.next();
+        System.out.println("Prompt the product visual :\n");
+        visual_prod = scan.next();
+        java.sql.Connection connection = Connection.connect();
+        try{
+            String requete = "INSERT INTO Produit(titre, visuel) VALUES(?, ?, ?, ?)";
+            PreparedStatement ps = connection.prepareStatement(requete);
+            ps.setString(1, name_prod);
+            ps.setString(2, description_prod);
+            ps.setString(3, price_prod);
+            ps.setString(4, visual_prod);
+            ps.executeUpdate();
+            connection.close();
+        } catch (SQLException sqle) {
+            System.out.println("FAIL " + sqle.getMessage());
+        }
+    }
+
+    public void edit_prod(){
+        String name_prod = null;
+        String description_prod = null;
+        String price_prod = null;
+        String visual_prod = null;
+        String id_prod = null;
+        System.out.println("Wich product would you change ?\n");
+        id_prod = scan.next();
+        System.out.println("Prompt the product name :\n");
+        name_prod = scan.next();
+        System.out.println("Prompt the product description :\n");
+        description_prod = scan.next();
+        System.out.println("Prompt the product price :\n");
+        price_prod = scan.next();
+        System.out.println("Prompt the product visual :\n");
+        visual_prod = scan.next();
+        java.sql.Connection connection = Connection.connect();
+        try{
+            String requete = "UPDATE Produit SET nom=?, description=?, tarif=?, visuel=? WHERE id_produit=? ";
+            PreparedStatement ps = connection.prepareStatement(requete);
+            ps.setString(1, name_prod);
+            ps.setString(2, description_prod);
+            ps.setString(3, price_prod);
+            ps.setString(4, visual_prod);
+            ps.setString(5, id_prod);
+            ps.executeUpdate();
+            connection.close();
+        } catch (SQLException sqle) {
+            System.out.println("Pb select" + sqle.getMessage());
+        }
+    }
 //
 //    public void all_prod(){
 //        try{
@@ -87,4 +90,4 @@
 //            System.out.println("Pb select" + sqle.getMessage());
 //        }
 //    }
-//}
+}
