e42ccae026915b5000cc4167e674f1147abbc5e8
Harsisis <cadetgauthier3@gmail.com>
2020-09-19 11:11:45 +0200

    trying to do commande

--- src/main/pojo/Commande.java
+++ src/main/pojo/Commande.java
@@ -1,17 +1,52 @@
 package main.pojo;
 
 import java.time.LocalDate;
-import java.util.HashMap;
+import java.util.List;
 
 public class Commande {
     private int id;
     private LocalDate date;
+    private int idClient;
 
-    //une commande est réalisé par un client :
-    private Client idClient;
+    private Map<Produit, Integer> produits;
 
-    //Ligne de commande : liste des produtis
-    //Une commande contient '1' ou 'n' produits
-    private HashMap<Produit, Integer> produitIntegerHashMap;
+    /* Méthode qui permet d'ajouter un produit à la ligne de commande. */
+    public void addProduit(Produit produit, Integer quantite) {
+        if (produits == null) {
+            produits = new HashMap<>();
+        }
+        produits.put(produit, quantite);
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public Map<Produit, Integer> getProduits() {
+        return produits;
+    }
+
+    public void setProduits(Map<Produit, Integer> produits) {
+        this.produits = produits;
+    }
 }
