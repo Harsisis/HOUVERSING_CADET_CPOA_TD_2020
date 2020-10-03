3b811cc2879ffb8170b3ee1776bde1f34b769664
yumamri <irma.houversing@gmail.com>
2020-10-03 10:51:44 +0200

    client test WORKS >:(

--- src/test/ListeMemoire/TestLMClient.java
+++ src/test/ListeMemoire/TestLMClient.java
@@ -1,24 +1,60 @@
 package test.ListeMemoire;
 
+import main.dao.fabrique.DAOFactory;
+import main.dao.fabrique.EPersistence;
+import main.dao.metiersDAO.ClientDAO;
 import main.pojo.Client;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestLMClient {
-
-    private int id = 1;
-    Client client;
+    private ClientDAO dao;
+    private EPersistence ePersistence = EPersistence.LISTEMEMORY;
 
     @Before
     public void setUp() {
-        Client client = null;
-
-        //Client clientTest = ListMemoireClientDAO.getInstance().create(client);
+        dao = DAOFactory.getDAOFactory(ePersistence).getClientDAO();
+        Assert.assertNotNull(dao);
+        Assert.assertNotNull(dao.findAll());
     }
+
     @Test
-    public void TestClientSQLGetById() {
-        System.out.println(client);
-        Assert.assertNotNull(client);
+    public void testFindByID() {
+        Client clientA = new Client();
+        //create a clientA
+        Assert.assertTrue(dao.create(clientA));
+        //create a clientB
+        Client clientB = dao.getById(clientA.getId());
+        //test if clientB is an instance of the class Client
+        Assert.assertTrue(clientB instanceof Client);
+    }
+
+    @Test
+    public void testCreateClient() {
+        //get the size of the array
+        int size = dao.findAll().size();
+        Client client = new Client();
+
+        Assert.assertTrue(dao.create(client));
+        //check if the array is incremented
+        Assert.assertEquals(size + 1, dao.findAll().size());
+    }
+
+    @Test
+    public void testUpdateClient() {
+        Client clientA = new Client();
+        clientA.setId(4);
+        clientA.setNom("This is a name");
+        //clientA is created
+        Assert.assertTrue(dao.create(clientA));
+        //test if the two products are the same
+        Client clientB = dao.getById(clientA.getId());
+        Assert.assertEquals(clientA,clientB);
+        //modifying clientB
+        clientB.setNom("Another one");
+        Assert.assertTrue(dao.update(clientB));
+        //verify
+        Assert.assertEquals(clientB,dao.getById(clientA.getId()));
     }
 }
