cbef1811a3b050d09f3531871860dc3efc149c85
Harsisis <56965488+Harsisis@users.noreply.github.com>
2020-10-14 12:27:15 +0200

    get the textfield content

--- src/main/ui/controller/controller_addProduit.java
+++ src/main/ui/controller/controller_addProduit.java
@@ -1,8 +1,26 @@
 package main.ui.controller;
 
+import com.gluonhq.charm.glisten.control.TextArea;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import main.pojo.Produit;
 
+import java.awt.event.MouseEvent;
+
 public class controller_addProduit {
+
+    @FXML
+    private TextField inputName;
+
+    @FXML
+    private TextField inputPrice;
+
+    @FXML
+    private TextArea inputDesc;
+
+    @FXML
+    private Label outputProduct;
 
     public boolean checkProd(String nom_prod, String desc_prod, float tarif_prod, String categ_prod){
         if (nom_prod!=""){
@@ -14,13 +32,15 @@ public class controller_addProduit {
         }
         return false;
     }
-
-    public void createProduct(){
+    @FXML
+    void onClickCreateProduct(MouseEvent event) {
         //read textfields to setup variables
-        String nom_prod = null;
+        String nom_prod = inputName.getText();
         String desc_prod = null;
         float tarif_prod = 0;
         String categ_prod = null;
+
+        outputProduct.setText(nom_prod);
 
         //optionnal for now but check the entries
         //checkProd(nom_prod, desc_prod, tarif_prod, categ_prod);
@@ -29,7 +49,6 @@ public class controller_addProduit {
         Produit produit = new Produit();
 
         //display in display label the newest product with toString()
-
 
     }
 
